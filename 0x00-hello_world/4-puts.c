89a146af39c7c26be608a8e616bdf0481ea5216c
Dericdero <dericashimwe@gmail.com>
2023-06-15 17:58:00 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with  puts function
- * return; always 0 (success)
+ * main - main Entry point
+ * return: Always 0 (Success)
  */
 int main(void)
 {
