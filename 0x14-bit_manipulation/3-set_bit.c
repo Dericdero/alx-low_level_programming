25312ea67326849bb2216d697e8087b7e05f1d95
Dericdero <dericashimwe@gmail.com>
2023-08-03 23:59:02 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,12 +10,12 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int)*8)
+	if (index >= sizeof(unsigned long int) * 8)
 	{
-		return(-1);
+		return (-1);
 	}
 
-	unsigned long int num = 1UL <<index;
+	unsigned long int num = 1UL << index;
 	*n |= num;
 
 	return (1);
